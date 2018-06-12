49f79bbdcdecf5b6d11e57294f099ba9d8ed01ee
meilin.ning <meilin.ning@weimob.com>
2018-06-12 23:51:50 +0800

    update

--- js/base.js
+++ js/base.js
@@ -1,17 +1,1 @@
-// 调整字体大小
-(function(doc, win) {
-    var docEl = doc.documentElement,
-        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        recalc = function () {
-            var baseFontSize = 100; // 根元素基准字体大小
-            var designWidth  = 750; // 设计稿宽度
-            var clientWidth = docEl.clientWidth;
-            if (!clientWidth) return;
-            docEl.style.fontSize = baseFontSize * (clientWidth / designWidth) + 'px';
-        };
 
-    // Abort if browser does not support addEventListener
-    if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvt, recalc, false);
-    doc.addEventListener('DOMContentLoaded', recalc, false);
-})(document, window);
