9261df3eaa899f6415e7192163b2aa02d3ea0ead
meilin.ning <meilin.ning@weimob.com>
2018-06-15 18:14:44 +0800

    update

--- js/base.js
+++ js/base.js
@@ -1,1 +1,18 @@
+var sheader = $('.s-header').outerHeight()
+$(document).scroll(function(){
+    var iStop = $(document).scrollTop()
+    iStop>sheader? $('html').addClass('nav-fixed'): $('html').removeClass('nav-fixed')
+})
 
+var mySwiper = new Swiper ('.swiper-container', {
+    loop: true, //循环播放
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    autoplay: {
+        delay: 2000,    //轮播切换时间
+        disableOnInteraction: false
+    },
+    speed: 1000
+})
